53d05dee0308975796b1417faed8fdbf8a8ce94e
Mohammad Faizan <faizancurious@gmail.com>
2024-07-11 00:50:52 +0530

    minor ui changes

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -6,7 +6,7 @@ export default async function HomePage() {
   return (
     <div className="h-[50rem] w-full dark:bg-black bg-white dark:bg-grid-white/[0.09] bg-grid-black/[0.05] relative flex items-center justify-center">
       {/* Radial gradient for the container to give a faded look */}
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_5%,black)]" />
 
       <div>
         <div className="flex flex-col">
