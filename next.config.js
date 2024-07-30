6c09a80469e2b38ea492b824fd3bfa3126377e91
Mohammad Faizan <faizancurious@gmail.com>
2024-07-30 13:16:51 +0530

    eslint | #2 fix

--- next.config.js
+++ next.config.js
@@ -3,58 +3,62 @@
  * for Docker builds.
  */
 await import("./src/env.js");
+const isProduction = process.env.NODE_ENV === "production";
 
 /** @type {import("next").NextConfig} */
 const config = {
-    // add images url
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "avatars.githubusercontent.com"
-            },
-            {
-                protocol: "https",
-                hostname: "cdn.discordapp.com"
-            },
-            {
-                protocol: "https",
-                hostname: "res.cloudinary.com"
-            },
-            {
-                protocol: "https",
-                hostname: "lh3.googleusercontent.com"
-            },
-            {
-                protocol: "https",
-                hostname: "pbs.twimg.com"
-            },
-            {
-                protocol: "https",
-                hostname: "i.imgur.com"
-            },
-            {
-                protocol: "https",
-                hostname: "i.ytimg.com"
-            },
-            {
-                protocol: "https",
-                hostname: "media.discordapp.net"
-            },
-            {
-                protocol: "https",
-                hostname: "cdn.discordapp.com"
-            },
-            {
-                protocol: "https",
-                hostname: "media.tenor.com"
-            },
-            {
-                protocol: "https",
-                hostname: "media1.tenor.com"
-            }
-        ],
-    },
+  // add images url
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media.discordapp.net",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media.tenor.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media1.tenor.com",
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: isProduction,
+  },
 };
 
 export default config;
