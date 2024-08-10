ad9d5ef164490241a3aa5c100f4c871ba0e7a575
Mohammad Faizan <faizancurious@gmail.com>
2024-08-10 12:23:04 +0530

    Loader

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,83 +1,42 @@
-  import React, { Suspense } from "react";
-  import type { Metadata, ResolvingMetadata } from "next";
-  import { getUserByUsername } from "~/server/user";
+import React, { Suspense } from "react";
+import type { Metadata, ResolvingMetadata } from "next";
+import { getUserByUsername } from "~/server/user";
+import { Loader } from "lucide-react";
 
-  const UsernamePage = React.lazy(() => import("./components/UserPage"));
+const UsernamePage = React.lazy(() => import("./components/UserPage"));
 
-  type Props = {
-    params: { username: string };
-  };
+type Props = {
+  params: { username: string };
+};
 
-  export async function generateMetadata(
-    { params }: Props,
-    parent: ResolvingMetadata,
-  ): Promise<Metadata> {
-    // read route params
-    const id = params.username;
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  // read route params
+  const id = params.username;
 
-    // fetch data
-    const user = await getUserByUsername(id);
+  // fetch data
+  const user = await getUserByUsername(id);
 
-    if (!user) {
-      return {
-        title: "Claim this username | Tielinks",
-        description:
-          "Claim this username for self | Tielinks | A fancy and cool link in bio | Share all links in one place",
-        icons: [
-          {
-            url: "https://tielinks.vercel.app/favicon.ico",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            url: "https://tielinks.vercel.app/favicon.ico",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            url: "https://tielinks.vercel.app/favicon.ico",
-            sizes: "1024x1024",
-            type: "image/png",
-          },
-        ],
-        applicationName: "Tielinks",
-        creator: "HelloFaizan",
-        twitter: {
-          site: "@tielinksgg",
-          creator: "@hellofaizaan",
-          card: "summary_large_image",
-          title: "User Not Found | Tielinks",
-          description:
-            "User Not Found | Tielinks | A fancy and cool link in bio | Share all links in one place",
-        },
-        openGraph: {
-          title: "User Not Found | Tielinks",
-          description:
-            "User Not Found | Tielinks | A fancy and cool link in bio | Share all links in one place",
-        },
-      };
-    }
-
+  if (!user) {
     return {
-      title: user?.name + "'s Profile | Tielinks",
+      title: "Claim this username | Tielinks",
       description:
-        user?.about +
-        " | " +
-        user?.name +
-        " is on Tielinks | A fancy and cool link in bio | Share all links in one place",
+        "Claim this username for self | Tielinks | A fancy and cool link in bio | Share all links in one place",
       icons: [
         {
-          url: user?.image || "https://tielinks.vercel.app/favicon.ico",
+          url: "https://tielinks.vercel.app/favicon.ico",
           sizes: "192x192",
           type: "image/png",
         },
         {
-          url: user?.image || "https://tielinks.vercel.app/favicon.ico",
+          url: "https://tielinks.vercel.app/favicon.ico",
           sizes: "512x512",
           type: "image/png",
         },
         {
-          url: user?.image || "https://tielinks.vercel.app/favicon.ico",
+          url: "https://tielinks.vercel.app/favicon.ico",
           sizes: "1024x1024",
           type: "image/png",
         },
@@ -88,26 +47,74 @@
         site: "@tielinksgg",
         creator: "@hellofaizaan",
         card: "summary_large_image",
-        title: user?.name + "'s Profile | Tielinks",
+        title: "User Not Found | Tielinks",
         description:
-          user?.about +
-          " | Tielinks | A fancy and cool link in bio | Share all links in one place",
+          "User Not Found | Tielinks | A fancy and cool link in bio | Share all links in one place",
       },
       openGraph: {
-        title: user?.name + "'s Profile | Tielinks",
+        title: "User Not Found | Tielinks",
         description:
-          user?.about +
-          " | Tielinks | A fancy and cool link in bio | Share all links in one place",
+          "User Not Found | Tielinks | A fancy and cool link in bio | Share all links in one place",
       },
     };
   }
 
-  // TODO: Suspense error if user not found due to generateMetadata
+  return {
+    title: user?.name + "'s Profile | Tielinks",
+    description:
+      user?.about +
+      " | " +
+      user?.name +
+      " is on Tielinks | A fancy and cool link in bio | Share all links in one place",
+    icons: [
+      {
+        url: user?.image || "https://tielinks.vercel.app/favicon.ico",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: user?.image || "https://tielinks.vercel.app/favicon.ico",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        url: user?.image || "https://tielinks.vercel.app/favicon.ico",
+        sizes: "1024x1024",
+        type: "image/png",
+      },
+    ],
+    applicationName: "Tielinks",
+    creator: "HelloFaizan",
+    twitter: {
+      site: "@tielinksgg",
+      creator: "@hellofaizaan",
+      card: "summary_large_image",
+      title: user?.name + "'s Profile | Tielinks",
+      description:
+        user?.about +
+        " | Tielinks | A fancy and cool link in bio | Share all links in one place",
+    },
+    openGraph: {
+      title: user?.name + "'s Profile | Tielinks",
+      description:
+        user?.about +
+        " | Tielinks | A fancy and cool link in bio | Share all links in one place",
+    },
+  };
+}
 
-  export default function App({ params }: { params: { username: string } }) {
-    return (
-      <Suspense fallback={<div>Loading...</div>}>
-        <UsernamePage params={params} />
-      </Suspense>
-    );
-  }
+// TODO: Suspense error if user not found due to generateMetadata
+
+export default function App({ params }: { params: { username: string } }) {
+  return (
+    <Suspense
+      fallback={
+        <div className="flex min-h-dvh items-center justify-center md:min-h-screen">
+          <Loader className="animate-spin" />{" "}
+        </div>
+      }
+    >
+      <UsernamePage params={params} />
+    </Suspense>
+  );
+}
