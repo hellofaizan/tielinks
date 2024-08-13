492dfe684616736f11718c34db87e7c527b1ec5e
Mohammad Faizan <faizancurious@gmail.com>
2024-08-13 15:18:46 +0530

    preview player pause play

--- src/components/Play.tsx
+++ src/components/Play.tsx
@@ -1,46 +1,34 @@
 "use client";
 
 import { Loader, Play, Pause } from "lucide-react";
-import React from "react";
+import React, { useState, useRef } from "react";
 
 export default function PlaySong({ url }: { url: string }) {
-  const [isPlaying, setIsPlaying] = React.useState(false);
-  const [isLoading, setIsLoading] = React.useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const audioPlayer = useRef<any>(null);
+
+  const togglePlayPause = () => {
+    const prevValue = isPlaying;
+    setIsPlaying(!prevValue);
+    if (!prevValue) {
+      audioPlayer?.current.play();
+    } else {
+      audioPlayer?.current.pause();
+    }
+
+    audioPlayer?.current.addEventListener("ended", () => {
+      setIsPlaying(false);
+    });
+  };
 
   return (
     <button
       className="absolute bottom-0 right-0 z-20 mb-[14px] mr-4 flex cursor-pointer items-center gap-1 rounded-full border border-green-500/40 bg-green-500/15 p-[1px] px-3 text-sm"
-      onClick={() => {
-        // play aayah audio from data.audio.primary
-        const audio = new Audio(url);
-        // if loadind show loading else show play icon
-        setIsLoading(true);
-        if (isPlaying === true) {
-          setIsPlaying(false);
-          setIsLoading(false);
-          audio.pause();
-          return;
-        } else {
-          if (audio.readyState === 4) {
-            setIsLoading(false);
-            setIsPlaying(true);
-            audio.play();
-          } else {
-            audio.oncanplay = () => {
-              setIsLoading(false);
-              setIsPlaying(true);
-              audio.play();
-            };
-          }
-        }
-
-        audio.onended = () => {
-          setIsLoading(false);
-          setIsPlaying(false);
-          return;
-        };
-      }}
+      onClick={togglePlayPause}
     >
+      <audio ref={audioPlayer} src={url} preload="metadata"></audio>
       {isLoading ? (
         <Loader className="animate-spin" size={15} />
       ) : isPlaying ? (
