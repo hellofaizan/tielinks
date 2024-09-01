831742969ccc245638f6124a5522290d6f787930
Mohammad Faizan <faizancurious@gmail.com>
2024-09-01 14:45:37 +0530

    oauth account linking

--- src/server/auth.config.ts
+++ src/server/auth.config.ts
@@ -1,21 +1,24 @@
-import GitHub from "next-auth/providers/github"
-import Discord from "next-auth/providers/discord"
-import Google from "next-auth/providers/google"
-import type { NextAuthConfig } from "next-auth"
+import GitHub from "next-auth/providers/github";
+import Discord from "next-auth/providers/discord";
+import Google from "next-auth/providers/google";
+import type { NextAuthConfig } from "next-auth";
 
 export default {
-    providers: [
-        GitHub({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        }),
-        Discord({
-            clientId: process.env.DISCORD_CLIENT_ID,
-            clientSecret: process.env.DISCORD_CLIENT_SECRET,
-        }),
-        Google({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        })
-    ]
-} satisfies NextAuthConfig
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
+    }),
+    Discord({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
+    }),
+  ],
+} satisfies NextAuthConfig;
